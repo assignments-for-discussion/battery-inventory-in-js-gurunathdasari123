f7858c013f9acc06092e5f52b9d33036af545638
gurunathdasari123 <gurunathdasari123@gmail.com>
2023-10-19 11:28:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
-  return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
-  };
+    // Initialize counters for healthy, exchange, and failed batteries
+    let healthy = 0;
+    let exchange= 0;
+    let failed = 0;
+
+    // Iterate through the array of present capacities
+    for (let i = 0; i < presentCapacities.length; i++) {
+        const ratedCapacity = 120;  // Rated capacity of a new battery
+        const soh = (presentCapacities[i] / ratedCapacity) * 100;
+
+        // Classify the battery based on SoH
+        if (soh > 80) {
+            healthyCount++;
+        } else if (soh >= 63 && soh <= 80) {
+            exchangeCount++;
+        } else {
+            failedCount++;
+        }
+    }
+
+    return [healthyCount, exchangeCount, failedCount];
 }
+
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
